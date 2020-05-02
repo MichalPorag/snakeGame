0c92003b2ed3ee10035bb9006acb661db8c2d7e5
Michal Porag <miporagi@gmail.com>
2020-05-02 14:13:01 +0300

    Updates

---
+++ precache-manifest.608daa85f5b464fc32a7435c195156fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc843e0e6afdaa5f1f78c31114fae4a7",
+    "url": "/snakeGame/index.html"
+  },
+  {
+    "revision": "3e463351366983e0f5f2",
+    "url": "/snakeGame/static/css/main.b4a78cfb.chunk.css"
+  },
+  {
+    "revision": "624ccec698abbb68049b",
+    "url": "/snakeGame/static/js/2.ebe5a245.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snakeGame/static/js/2.ebe5a245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e463351366983e0f5f2",
+    "url": "/snakeGame/static/js/main.af0814e5.chunk.js"
+  },
+  {
+    "revision": "67865af248df2b24f052",
+    "url": "/snakeGame/static/js/runtime-main.44a9b99f.js"
+  },
+  {
+    "revision": "879e8174ed3dbbfa4153cca57204cfa8",
+    "url": "/snakeGame/static/media/apple.879e8174.png"
+  },
+  {
+    "revision": "049edd82e59b94d48bfd4af952843bca",
+    "url": "/snakeGame/static/media/ocean.049edd82.jpg"
+  }
+]);
